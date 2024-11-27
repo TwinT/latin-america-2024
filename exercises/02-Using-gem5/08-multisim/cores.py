b5c6bf0f2672cef89ef81f92881bde58a3aaceb0
Marcos Cervetto <cervettomarcos@gmail.com>
2024-11-27 16:24:39 +0000

    changes

---
+++ exercises/02-Using-gem5/08-multisim/cores.py
@@ -0,0 +1,42 @@
+from gem5.components.boards.simple_board import SimpleBoard
+from gem5.components.processors.simple_processor import SimpleProcessor
+from gem5.components.cachehierarchies.classic.private_l1_cache_hierarchy import PrivateL1CacheHierarchy
+from gem5.components.memory.single_channel import SingleChannelDDR4_2400
+from gem5.components.processors.cpu_types import CPUTypes
+from gem5.isas import ISA
+from gem5.resources.resource import obtain_resource
+from gem5.simulate.simulator import Simulator
+import gem5.utils.multisim as multisim
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("cpu_type", type=str, help="The cpu type")
+
+args = parser.parse_args()
+
+from my_processor import BigProcessor, LittleProcessor
+if args.cpu_type == "Big":
+    processor = BigProcessor()
+elif args.cpu_type == "Little":
+    processor = LittleProcessor()
+elif args.cpu_type == "Simple":
+    processor = SimpleProcessor(cpu_type=CPUTypes.TIMING,
+                            isa=ISA.RISCV,
+                            num_cores=1)
+
+multisim.set_num_processes(2)
+for processor_type in [BigProcessor, LittleProcessor]:
+    for benchmark in obtain_resource("riscv-getting-started-benchmark-suite"):
+        board = SimpleBoard( clk_freq="3GHz",
+            processor=processor_type(), memory=SingleChannelDDR4_2400("1GiB"),
+            cache_hierarchy=PrivateL1CacheHierarchy(
+                l1d_size="32KiB", l1i_size="32KiB"
+            ),
+        )
+        board.set_workload(benchmark)
+        simulator = Simulator(
+            board=board, id=f"{processor_type.get_name()}-{benchmark.get_id()}"
+        )
+        multisim.add_simulator(simulator)
+
